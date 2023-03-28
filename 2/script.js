0ee76922d383fad59e8d8628cd64d632408c199e
ivander <gregoriusivander888@gmail.com>
2023-03-28 15:19:54 +0700

    flag

--- 2/script.js
+++ 2/script.js
@@ -1,18 +1,14 @@
 function GGWP() {
     var inputString = document.getElementById("inputString").value;
-    var randomBoolean = Math.random() < 0.5; 
+    var flags = ["klik 1 kali lagi", "seculab{U53r_49enT_5tR1n9}"]; 
+    var randomFlag = flags[Math.floor(Math.random() * flags.length)]; 
     var resultElement = document.getElementById("result");
     var colors = ["#FF5733", "#FFC300", "#33FF57", "#33E0FF", "#7A33FF"]; 
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-  
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]; 
+
     document.body.style.backgroundColor = randomColor; 
-  
-    if (randomBoolean) {
-      resultElement.innerHTML = "\"" + inputString + "\" is true!";
-      resultElement.style.color = "green"; 
-    } else {
-      resultElement.innerHTML = "\"" + inputString + "\" is false!";
-      resultElement.style.color = "red"; 
-    }
-  }
-  
+
+    resultElement.innerHTML = "\"" + inputString + "\" flag is: " + randomFlag;
+    resultElement.style.color = "black";
+
+}
